72202f92d3faf8ed63affa67dff9ad9f01aecf32
RemarkableGuy <alvaro.schipper@hotmail.com>
2020-02-09 18:37:38 +0100

    Added Container Put loading indicator and error message inside container Made App Bar sticky Added title Removed option arrays unshifts to add empty select values Changed selects to Material UI components Added default empty option to selects Changed TankList to use table Added tank avatars

--- src/filter/TankFilterOptions.js
+++ src/filter/TankFilterOptions.js
@@ -1,38 +1,72 @@
 import React from "react";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import FormControl from "@material-ui/core/FormControl";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+
+const useStyles = makeStyles(theme => ({
+    formControl: {
+        margin: theme.spacing(1),
+        width: "20%",
+    },
+
+}));
 
 function TankFilterOptions(props) {
+    const classes = useStyles();
+
     const handleChange = (type, event) => {
         const value = event.target.value;
         props.changeOption(value, type);
     };
 
     return (
-        <div>
-            <label>Type</label>
-            <select value={props.type} onChange={event => handleChange('type', event)}>
-                {props.typeOptions.map(option =>
-                    <option key={option} value={option}>{option}</option>
-                )}
-            </select>
-            <label>Country</label>
-            <select value={props.country} onChange={event => handleChange('country', event)}>
-                {props.countryOptions.map(option =>
-                    <option key={option} value={option}>{option}</option>
-                )}
-            </select>
-            <label>Name</label>
-            <select value={props.name} onChange={event => handleChange('name', event)}>
-                {props.nameOptions.map(option =>
-                    <option key={option} value={option}>{option}</option>
-                )}
-            </select>
-            <label>Ammunition</label>
-            <select value={props.ammunition} onChange={event => handleChange('ammunition', event)}>
-                {props.ammunitionOptions.map(option =>
-                    <option key={option} value={option}>{option}</option>
-                )}
-            </select>
-        </div>
+        <Card>
+            <CardHeader title="Filter"/>
+            <CardContent>
+                <FormControl className={classes.formControl}>
+                    <InputLabel id="type">Type</InputLabel>
+                    <Select value={props.type} onChange={event => handleChange('type', event)} labelId="type">
+                        <MenuItem value="">None</MenuItem>
+                        {props.typeOptions.map(option =>
+                            <MenuItem key={option} value={option}>{option}</MenuItem>
+                        )}
+                    </Select>
+                </FormControl>
+                <FormControl className={classes.formControl}>
+                    <InputLabel id="country">Country</InputLabel>
+                    <Select value={props.country} onChange={event => handleChange('country', event)} labelId="country">
+                        <MenuItem value="">None</MenuItem>
+                        {props.countryOptions.map(option =>
+                            <MenuItem key={option} value={option}>{option}</MenuItem>
+                        )}
+                    </Select>
+                </FormControl>
+                <FormControl className={classes.formControl}>
+                    <InputLabel id="name">Name</InputLabel>
+                    <Select value={props.name} onChange={event => handleChange('name', event)} labelId="name">
+                        <MenuItem value="">None</MenuItem>
+                        {props.nameOptions.map(option =>
+                            <MenuItem key={option} value={option}>{option}</MenuItem>
+                        )}
+                    </Select>
+                </FormControl>
+                <FormControl className={classes.formControl}>
+                    <InputLabel id="ammunition">Ammunition</InputLabel>
+                    <Select value={props.ammunition} onChange={event => handleChange('ammunition', event)}
+                            labelId="ammunition">
+                        <MenuItem value="">None</MenuItem>
+                        {props.ammunitionOptions.map(option =>
+                            <MenuItem key={option} value={option}>{option}</MenuItem>
+                        )}
+                    </Select>
+                </FormControl>
+            </CardContent>
+        </Card>
     );
 }
 
